a458f9ce33d1ddd02e2598adae1243de79c8f23e
Roberto Delgado <robertodelgado@Robertos-MacBook-Air.local>
2020-01-23 17:58:20 -0800

    MVP reached

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -5,7 +5,7 @@ export const useDarkMode = () => {
   const [someValue, setSomeValue] = useLocalStorage(someValue);
 
   useEffect(() => {
-    return someValue ? document.body.className = 'dark-mode' : document.body.className = '';
+    someValue ? document.body.className = 'dark-mode' : document.body.className = '';
   }, [someValue]);
 
   return [someValue, setSomeValue];
