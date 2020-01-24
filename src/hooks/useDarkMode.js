c3be68bf760c1d4772ed37c80750652a6cab861f
Roberto Delgado <robertodelgado@Robertos-MacBook-Air.local>
2020-01-23 17:41:11 -0800

    Completed step 3, but toggle only works after refresh

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,4 +1,13 @@
-import useLocalStorage from './useLocalStorage'
+import { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
 
-export const useDarkMode = () => {};
+export const useDarkMode = () => {
+  const [someValue, setSomeValue] = useLocalStorage(someValue);
+
+  useEffect(() => {
+    return someValue ? document.body.className = 'dark-mode' : document.body.className = '';
+  }, [someValue]);
+
+  return [someValue, setSomeValue];
+};
 
